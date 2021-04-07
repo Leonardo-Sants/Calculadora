d1989ddd3aad1c9e67a3abac4792ba66bc8e8b2c
Leonardo dos Santos <Leonardo-Sants@github.com>
2021-04-07 20:09:26 -0300

    Criando index

---
+++ script.js
@@ -0,0 +1,22 @@
+const forms = document.querySelector('.formulario');
+const resultado = document.querySelector('.resultado');
+
+forms.addEventListener ('submit', function (e) {
+    e.preventDefault();
+
+    const primeiro = e.target.querySelector('.primeiro');
+    const segundo = e.target.querySelector('.segundo');
+
+    const pri = Number (primeiro.value);
+    const seg = Number (segundo.value);
+
+    const soma = getSoma(pri, seg);
+    resultado.innerHTML = (`Total ${soma}`)
+})
+
+function getSoma(x,y) {
+    const soma = x + y;
+    return soma.toFixed(2);
+}
+
+console.log(getSoma(2,5))
